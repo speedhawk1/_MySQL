779c7c0cc952af647621df1a8b5006c685bec356
thu <mingfei.net@gmail.com>
2016-10-29 16:20:00 +0800

    Update

--- day1/test1.sql
+++ day1/test1.sql
@@ -2,14 +2,31 @@ SHOW DATABASES;
 
 USE test;
 
-SHOW TABLES ;
+SHOW TABLES;
 
 CREATE TABLE student (
-  sno VARCHAR(10),
+  sno   VARCHAR(10),
   sname VARCHAR(20),
-  age INT(3)
+  age   INT(3)
 );
 
-SELECT * FROM student;
+-- SQL select *****
 
+-- retrieve
+SELECT *
+FROM student;
+
+-- create
 INSERT INTO student VALUES ('001', 'zhangsan', 18);
+INSERT INTO student VALUES ('002', 'lisi', 20);
+
+SHOW VARIABLES LIKE 'char%';
+SHOW VARIABLES LIKE 'coll%';
+
+-- update
+UPDATE student
+SET age = 19
+WHERE sname = 'lisi';
+
+-- delete
+DELETE FROM student WHERE sno = '002';
