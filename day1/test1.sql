81b1972242988305236999fce395cf6589a26461
thu <mingfei.net@gmail.com>
2016-10-30 10:23:16 +0800

    Update

--- day1/test1.sql
+++ day1/test1.sql
@@ -1,10 +1,12 @@
-SHOW DATABASES;
+# SHOW DATABASES;
 
-USE test;
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
 
-SHOW TABLES;
+# SHOW TABLES FROM test;
 
-CREATE TABLE student (
+DROP TABLE IF EXISTS test.student;
+CREATE TABLE test.student (
   sno   VARCHAR(10),
   sname VARCHAR(20),
   age   INT(3)
@@ -14,19 +16,29 @@ CREATE TABLE student (
 
 -- retrieve
 SELECT *
-FROM student;
+FROM test.student;
 
 -- create
-INSERT INTO student VALUES ('001', 'zhangsan', 18);
-INSERT INTO student VALUES ('002', 'lisi', 20);
+INSERT INTO test.student VALUES ('001', 'zhangsan', 18);
+INSERT INTO test.student VALUES ('002', 'lisi', 20);
+INSERT INTO test.student VALUES ('003', '中文', 20);
 
-SHOW VARIABLES LIKE 'char%';
-SHOW VARIABLES LIKE 'coll%';
+# SHOW VARIABLES LIKE 'char%';
+# SHOW VARIABLES LIKE 'coll%';
 
 -- update
-UPDATE student
+UPDATE test.student
 SET age = 19
 WHERE sname = 'lisi';
 
 -- delete
-DELETE FROM student WHERE sno = '002';
+DELETE FROM test.student
+WHERE sno = '002';
+
+
+# SHOW CREATE DATABASE test;
+
+
+# SHOW CREATE TABLE test.student;
+
+SELECT 1 /* test... */+1;
