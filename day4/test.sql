b63de286f4633e749a1fdcb65fc0d8c4bd26a801
thu <mingfei.net@gmail.com>
2016-11-06 14:43:06 +0800

    Update

--- day4/test.sql
+++ day4/test.sql
@@ -22,4 +22,41 @@ FROM scott.emp;
 
 -- ----------------------
 DELETE FROM scott.emp; -- commit;
-ROLLBACK ;
+ROLLBACK;
+
+DROP TABLE scott.t;
+
+CREATE TABLE scott.t (
+  id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+INSERT INTO scott.t VALUE (NULL);
+
+SELECT *
+FROM scott.t;
+
+DELETE FROM scott.t;
+
+ALTER TABLE scott.t
+  AUTO_INCREMENT = 1;
+
+SELECT
+  min(ENAME),
+  max(HIREDATE),
+  round(avg(ENAME), 2),
+  sum(ENAME),
+  count(SAL)
+FROM scott.emp;
+
+SELECT DEPTNO, avg(SAL)
+FROM scott.emp
+GROUP BY DEPTNO;
+
+
+SELECT JOB, DEPTNO, avg(SAL + ifnull(COMM, 0))
+FROM scott.emp
+GROUP BY JOB, DEPTNO
+HAVING avg(SAL + ifnull(COMM, 0)) > 2000; -- 组检索 ：having  where ： 行检索
+
+SELECT *
+FROM scott.emp;
