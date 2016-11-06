98a52c3733ab1f2c17c1020733cb2bc8ee1803d5
thu <mingfei.net@gmail.com>
2016-11-06 11:48:21 +0800

    Update

--- day4/school.sql
+++ day4/school.sql
@@ -1,7 +1,14 @@
-CREATE DATABASE db_school;
+CREATE DATABASE IF NOT EXISTS db_school;
+
+DROP TABLE IF EXISTS db_school.school;
 CREATE TABLE db_school.school (
   title    VARCHAR(255),
   admin    VARCHAR(255),
   location VARCHAR(255),
   level    VARCHAR(255)
 );
+
+SELECT *
+FROM db_school.school;
+TRUNCATE TABLE db_school.school;
+
