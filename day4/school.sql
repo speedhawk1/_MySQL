b02cab37ab2902787129b43f31b96d196c9c7d5c
thu <mingfei.net@gmail.com>
2016-11-06 13:06:23 +0800

    Update

--- day4/school.sql
+++ day4/school.sql
@@ -2,13 +2,24 @@ CREATE DATABASE IF NOT EXISTS db_school;
 
 DROP TABLE IF EXISTS db_school.school;
 CREATE TABLE db_school.school (
+  id INT AUTO_INCREMENT PRIMARY KEY ,
   title    VARCHAR(255),
   admin    VARCHAR(255),
   location VARCHAR(255),
   level    VARCHAR(255)
 );
 
+ALTER TABLE db_school.school
+  ADD id INT AUTO_INCREMENT PRIMARY KEY
+  FIRST;
+
 SELECT *
 FROM db_school.school;
+
 TRUNCATE TABLE db_school.school;
 
+
+LOAD DATA INFILE 'd:/0/idea_projects/_mysql/day4/school.csv'
+INTO TABLE db_school.school
+FIELDS TERMINATED BY ',' (title, admin, location, level)
+SET id = NULL ;
--- day4/test.sql
+++ day4/test.sql
@@ -1,12 +1,18 @@
-
-
-START TRANSACTION ;
+START TRANSACTION;
 
 SELECT *
 FROM scott.emp;
 
-DELETE FROM scott.emp;
+DELETE
+FROM scott.emp
+WHERE EMPNO = 7521; -- DML
 
 # TRUNCATE TABLE scott.emp;
 
-ROLLBACK ; -- 回滚
+ROLLBACK; -- 回滚
+
+COMMIT; -- 提交
+
+CREATE TABLE scott.t( -- DDL
+  id INT
+);
